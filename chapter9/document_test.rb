4933dbd19e665a4940522f386db25a115abf1f29
chuylerma <4555734-chuylerma@users.noreply.gitlab.com>
2020-07-31 23:02:04 -0500

    Add test_that_doc_can_return_words_in_array

--- chapter9/document_test.rb
+++ chapter9/document_test.rb
@@ -4,8 +4,17 @@ require './document.rb'
 
 class DocumentTest < Test::Unit::TestCase
   def test_document_holds_onto_contents
-    text = "A bunch of words"
+    text = 'A bunch of words'
     doc = Document.new('test', 'nobody', text)
     assert_equal text, doc.content, 'Contents are still there'
+  end
+
+  def test_that_doc_can_return_words_in_array
+    text = 'A bunch of words'
+    doc = Document.new('test', 'nobody', text)
+    assert doc.words.include?( 'A' )
+    assert doc.words.include?( 'bunch' )
+    assert doc.words.include?( 'of' )
+    assert doc.words.include?( 'words' )
   end
 end
