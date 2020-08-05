bb7b840b24a56de059d004c667b958e0dad6bae0
chuylerma <4555734-chuylerma@users.noreply.gitlab.com>
2020-08-04 23:51:28 -0500

    Add notes

--- chapter10/chapter.rb
+++ chapter10/chapter.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
 puts 'Construct Your Classes from Short, Focused Methods!'
 
+puts <<EOF
+We have applied the composed method technice. This technice
+advocates dividing your class up into methods that have
+three characteristics. First, each method should do a single
+thing - focus on solving a single aspect of the problem. By
+
+Second, each method needs to operate at a single conceptual level:
+Simply put, don't mix high-level logic with details.
+
+Finally, each method needs to have a name that reflects its
+purpose.
+EOF
 
