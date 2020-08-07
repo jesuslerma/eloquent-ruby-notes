200196571c003fbf8b0dbeab4c5dc7b3686712a1
chuylerma <4555734-chuylerma@users.noreply.gitlab.com>
2020-08-06 20:33:29 -0500

    Remove files from chapter 10

--- chapter10/printable_document.rb
+++
@@ -1,9 +0,0 @@
-class PrintableDocument < Document
-  def print( printer )
-    return 'Printer unavailable' unless printer.available?
-    printer.render( "#{title}\n" )
-    printer.render( "By #{author}\n" )
-    printer.render( content )
-    'Done'
-  end
-end
