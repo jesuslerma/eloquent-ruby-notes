2bc04797f5ac241c72238ef75e9ab8c689884ba0
chuylerma <4555734-chuylerma@users.noreply.gitlab.com>
2020-08-06 20:54:22 -0500

    Add '+' operator to Document

---
+++ chapter11/document.rb
@@ -0,0 +1,55 @@
+class Document 
+  attr_accessor :title, :author, :content
+
+  def initialize( title, author, content )
+    @title = title
+    @author = author
+    @content = content
+  end
+
+  def words
+    @content.split
+  end
+
+  def word_count
+    words.size
+  end
+
+  def prose_rating
+    return :really_pretentious if really_pretentious?
+    return :somewhat_pretentious if somewhat_pretentious?
+    return :really_informal if really_informal?
+    return :somewhat_informal if somewhat_informal?
+  end
+
+  def really_pretentious?
+    pretentious_density > 0.3 && informal_density < 0.2
+  end
+
+  def somewhat_pretentious?
+    pretentious_density > 0.3 && informal_density >= 0.2
+  end
+
+  def really_informal?
+    pretentious_density < 0.1 && informal_density > 0.3
+  end
+
+  def somewhat_informal?
+    pretentious_density < 0.1 && informal_density <= 0.3
+  end
+
+  def pretentious_density
+    # fake this method
+    0.4
+  end
+
+  def informal_density
+    # fake this method
+    0.3
+  end
+
+  def +(other)
+    Document.new( title, author, "#{content} #{other.content}" )
+  end
+end
+
---
+++ chapter11/document_spec.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+require './document.rb'
+
+describe Document do
+  before :each do
+    @text = 'A bunch of words'
+    @doc = Document.new( 'test', 'nobody', @text )
+  end
+  it 'should hold on to the contents' do
+    @doc.content.should == @text
+  end
+
+  it 'should know which words it has' do
+    @doc.words.should include( 'A' )
+    @doc.words.should include( 'bunch' )
+    @doc.words.should include( 'of' )
+    @doc.words.should include( 'words' )
+  end
+
+  it 'should know how many words it contains' do
+    @doc.word_count.should == 4
+  end
+
+  it 'should add a Document' do
+    doc2 = Document.new('Tag Line1', 'Kirk', 'of the star ship')
+    total_document = @doc + doc2
+    total_document.content.should == 'A bunch of words of the star ship'
+  end
+end
