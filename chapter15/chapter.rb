85ff0175bd590ed369547416ed5e41de4d520a31
jesuslerma <shuyojl@gmail.com>
2020-08-20 22:51:53 -0500

    Finish chapter15

--- chapter15/chapter.rb
+++ chapter15/chapter.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/env ruby
 
 require './font.rb'
+require './tons_o_toner.rb'
+require './oceans_of_ink.rb'
+
 puts 'Use modules as name spaces'
+
+use_lase_printer = true
+
+if use_lase_printer
+  print_module = TonsOToner
+else
+  print_module = OceansOfInk
+end
+
+admin = print_module::Administration.new
+admin.test
+
+puts 'you gan dig into this gem https://github.com/rom-rb/rom'
