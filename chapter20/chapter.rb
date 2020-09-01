efe6f7b8ac2e74561ff19b8a07208db3bf088ab1
jesuslerma <shuyojl@gmail.com>
2020-08-31 22:46:58 -0500

    Add subclass hook sample

--- chapter20/chapter.rb
+++ chapter20/chapter.rb
@@ -1,40 +1,13 @@
 #!/usr/bin/env ruby
-require './document.rb'
-require './block_based_archival_document.rb'
-require 'net/http'
 
-puts 'Save blocks to execute later'
+puts 'Use Hooks to Keep Your Program Informed'
 
-def run_that_block( &that_block )
-  puts 'About to run that block'
-  that_block.call if that_block
-  puts 'Done running the block'
+class SimpleBaseClass
+  def self.inherited( new_subclass )
+    puts "Hey #{new_subclass} is now a sublcass of #{self}!"
+  end
 end
 
-run_that_block do
-  puts 'We are in the block'
+class ChildClassOne < SimpleBaseClass
 end
 
-my_doc = Document.new( 'Block Based Example', 'russ', '' )
-
-#my_doc.on_load do |doc|
-#  puts "Hey, I've been loaded!"
-#end
-
-#my_doc.on_save do |doc|
-#  puts "Hey, I've been saved!"
-#end
-
-my_doc.load('example.txt')
-my_doc.save('example.txt')
-
-# this is a simple example of how can we work.
-# file_doc = BlockBasedArchivalDocument.new( 'file', 'russ') do
-#   File.read('some_text.txt')
-# end
-
-google_doc = BlockBasedArchivalDocument.new('http', 'russ') do
-  Net::HTTP.get_response('www.google.com', 'index.html').body
-end
-
-puts google_doc.content
