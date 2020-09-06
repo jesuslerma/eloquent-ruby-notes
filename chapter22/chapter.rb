b4d7fbb730c367d496068a62ffa853f80892e9a6
jesuslerma <shuyojl@gmail.com>
2020-09-05 23:55:38 -0500

    Add super secret document base

--- chapter22/chapter.rb
+++ chapter22/chapter.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 require './document.rb'
+require './super_secret_document.rb'
 
 puts 'Use method_missing for Delegation'
 
+def get_instructions
+  return Document.new( 'Cien años de Soledad', 'Gabriel Garcia MArquez', 'Contenido de la familia buen dia')
+end
+
+original_instructions = get_instructions
+
+instructions = SuperSecretDocument.new(original_instructions, 5)
---
+++ chapter22/super_secret_document.rb
@@ -0,0 +1,30 @@
+class SuperSecretDocument
+  def initialize( original_document, time_limit_seconds )
+    @original_document = original_document
+    @time_limit_seconds = time_limit_seconds
+    @create_time = Time.now
+  end
+
+  def time_expired?
+    Time.now - @create_time >= @time_limit_seconds
+  end
+
+  def check_for_expiration
+    raise 'Document no longer available' if time_expired?
+  end
+
+  def content
+    check_for_expiration
+    return @original_document.content
+  end
+
+  def title
+    check_for_expiration
+    return @original_document.title
+  end
+
+  def author
+    check_for_expiration
+    return @original_document.author
+  end
+end
