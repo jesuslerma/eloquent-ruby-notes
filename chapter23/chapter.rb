86098a5e243a043b220de39f2d2ddcda8a207958
jesuslerma <shuyojl@gmail.com>
2020-09-09 18:14:53 -0500

    Generate magic methods from method_missing

--- chapter23/chapter.rb
+++ chapter23/chapter.rb
@@ -6,13 +6,14 @@ puts 'Use method missing to Build Flexible Apis'
 
 offer_letter = FormLetter.new("Special Offer", "Acme Inc",
   %q{
-    Dear Mr. LASTNAME
+    Dear GENDER LASTNAME
     Are you troubled byt the
     ...
     FIRSTNAME, we loook forward to hearing from you.
 })
 
-offer_letter.replace_word('FIRSTNAME', 'Chuy')
-offer_letter.replace_word('LASTNAME', 'Lerma')
+offer_letter.replace_firstname('Chuy')
+offer_letter.replace_lastname('Lerma')
+offer_letter.replace_gender('Mr.')
 
 puts offer_letter.content
--- chapter23/form_letter.rb
+++ chapter23/form_letter.rb
@@ -1,5 +1,26 @@
+# This class uses magic methods from method_missing
 class FormLetter < Document
   def replace_word( old_word, new_word )
     @content.gsub!( old_word, "#{new_word}" )
+  end
+
+  def method_missing( name, *args )
+    string_name = name.to_s
+    return super unless string_name =~ /^replace_\w+/
+    old_word = extract_old_word(string_name)
+    replace_word( old_word, args.first )
+  end
+
+  def extract_old_word( name )
+    name_parts = name.split('_')
+    name_parts[1].upcase
+  end
+
+  def replace_firstname( new_first_name )
+    replace_word('FIRSTNAME', new_first_name)
+  end
+
+  def replace_lastname( new_last_name )
+    replace_word('LASTNAME', new_last_name)
   end
 end
