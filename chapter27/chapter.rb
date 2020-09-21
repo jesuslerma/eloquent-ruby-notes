f87c99ddc076b294b2a2941c5d522912df2e4fc1
jesuslerma <shuyojl@gmail.com>
2020-09-20 19:47:59 -0500

    Fix author value

--- chapter27/chapter.rb
+++ chapter27/chapter.rb
@@ -10,3 +10,13 @@ end
 
 ripper.run( 'fellowship.xml' )
 
+
+mripper = XmlRipper.new do |r|
+  r.on_path( '/document/author/' ) do |author|
+    author.text = 'J.R.R. Tolkien'
+  end
+  r.after { |doc| puts doc }
+end
+
+mripper.run( 'fellowship.xml' )
+
